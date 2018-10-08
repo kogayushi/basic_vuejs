ce00bdbd67cc94b47846043e39853d1a5ff631f9
yushi koga <youshe.old.pleasure@gmail.com>
2018-10-08 20:56:18 +0900

    page 369

--- kanban-app/src/router/index.js
+++ kanban-app/src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import KbnLoginView from '@/components/templates/KbnLoginView.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/login',
+      component: KbnLoginView
     }
   ]
 })
